4f403f2915fe0b1ec7c75867b2fe110dcedeb0aa
okore431 <olussskin@gmail.com>
2023-01-25 03:05:43 +0000

    Project completed

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,48 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+const lightDurations = {
+	red: 3000,
+	yellow: 2000,
+	green: 1000
+  };
 //create your first component
 const Home = () => {
+	const[selectedColor, setSelectedColor] = useState ("red");
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+		  if (selectedColor === "red") {
+			setSelectedColor("yellow");
+		  } else if (selectedColor === "yellow") {
+			setSelectedColor("green");
+		  } else if (selectedColor === "green") {
+			setSelectedColor("red");
+		  }
+		}, lightDurations[selectedColor]);
+	  
+		return () => {
+		  clearTimeout(timeout);
+		};
+	  }, [selectedColor]);
+    
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<>
+		<div className="traffic-light">
+			<div
+			   onClick={() => setSelectedColor ("red")}
+			   className={"light red" + ((selectedColor === "red") ? " glow" : "")}>
+			</div>
+			<div  
+			   onClick={() => setSelectedColor ("yellow")}
+			   className={"light yellow" + ((selectedColor === "yellow") ? " glow" : "")}>
+			</div>
+			<div 
+			   onClick={() => setSelectedColor ("green")}
+			   className={"light green" + ((selectedColor === "green") ? " glow" : "")}>
+			</div>
 		</div>
+		</>
 	);
 };
 
 export default Home;
+
